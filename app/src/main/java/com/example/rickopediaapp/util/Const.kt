3b6e54aaaa289a2fd9a09ce810d4bfa75a5f8348
jorchar <jordan.kucharski@icloud.com>
2023-06-20 19:06:33 +0200

    Added and implemented RemoteMediator for Episode.

--- app/src/main/java/com/example/rickopediaapp/util/Const.kt
+++ app/src/main/java/com/example/rickopediaapp/util/Const.kt
@@ -1,6 +1,7 @@
 package com.example.rickopediaapp.util
 
 const val URL_SERVER = "https://rickandmortyapi.com/api/"
-const val PAGE_SIZE = 20
-const val PREFETCH_DISTANCE = PAGE_SIZE * 2
-const val ROUND_CORNER = 30
+const val CHARACTER_PAGE_SIZE = 20
+const val CHARACTER_PREFETCH_DISTANCE = CHARACTER_PAGE_SIZE * 2
+const val EPISODE_PAGE_SIZE = 4
+const val EPISODE_PREFETCH_DISTANCE = EPISODE_PAGE_SIZE * 2
